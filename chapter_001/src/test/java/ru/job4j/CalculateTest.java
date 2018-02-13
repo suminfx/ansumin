2d259dafa6450a5cb13e4d3a4ed132400b107bc1
suminfx <sumin931@yandex.ru>
2018-02-13 23:06:23 +0300

    1.4. JUnit

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Andrey Sumin (suminfx@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+public class CalculateTest {
+		/**
+		* Test echo.
+		*/
+		@Test
+		public void whenTakeNameThenThreeEchoPlusName(){
+			String input = "Andrey Sumin";
+			String expect = "Echo, echo, echo Andrey Sumin"; 
+			Calculate calc = new Calculate();
+			String result = calc.echo(input);
+			assertThat(result, is(expect));
+		}
+}
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate test.
+*
+* @author Andrey Sumin (suminfx@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
