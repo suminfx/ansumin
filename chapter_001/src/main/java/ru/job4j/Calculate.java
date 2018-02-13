46cdba38afe3cdc5d074d79af72a7c0d3660212e
suminfx <sumin931@yandex.ru>
2018-02-13 21:31:14 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j;
 
+/**
+* Calculate Класс для вывода на экран HelloWorld
+* @author asumin
+* @since 13.02.2018
+* @version 1
+*/
+
 public class Calculate {
+	/**
+	* Вывод на экран "Hello world!"
+	* @params args - аргументы командной строки
+	*/
 	public static void main(String[] args){
-		System.out.println("Hello world!");
+		System.out.println("Hello worlds!");
 	}
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Andrey Sumin (suminfx@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
