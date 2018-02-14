a6074253d6cf86602183673ae50a9af1b88c2d85
suminfx <sumin931@yandex.ru>
2018-02-14 23:09:29 +0300

    2.3. Импорт Maven проекта

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,27 +1,27 @@
 package ru.job4j;
 
 /**
-* Calculate Класс для вывода на экран HelloWorld
-* @author asumin
-* @since 13.02.2018
-* @version 1
-*/
+ * Calculate Класс для вывода на экран HelloWorld
+ * @author asumin
+ * @since 13.02.2018
+ * @version 1
+ */
 
 public class Calculate {
 	/**
-	* Вывод на экран "Hello world!"
-	* @params args - аргументы командной строки
-	*/
+	 * Вывод на экран "Hello world!"
+	 * @param args - аргументы командной строки
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello worlds!");
 	}
-	
+
 	/**
-	* Method echo
-	* @param name - Your name
-	* return Echo plus your name
-	*/
-	public String echo(String name) {
+	 * Method echo
+	 * @param name - Your name
+	 * return Echo plus your name
+	 */
+	 public String echo(String name) {
 		return "Echo, echo, echo " + name;
 	}
 }
