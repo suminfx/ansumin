d49bb0d78164419983cbc0ee861eb9a46fe5a9fb
suminfx <sumin931@yandex.ru>
2018-02-18 21:26:38 +0300

    В факториале удалил лишнее условие.

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -15,9 +15,7 @@ public class Factorial {
      */
     public int calc(int n) {
         int result = 1;
-        if (n == 0) {
-            return 1;
-        }
+
         for (int i = 1; i <= n; i++) {
             result *= i;
         }
