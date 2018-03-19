87d5c682eaca4930d233a078f2551b554aea7531
suminfx <sumin931@yandex.ru>
2018-03-19 18:58:23 +0300

    #10036 2. Сортировка User с использованием Comparator

--- chapter_002/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_002/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,8 +1,6 @@
 package ru.job4j.sort;
 
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Класс для сортировки пользователей по возрасту в порядке возрастания.
@@ -22,5 +20,57 @@ public class SortUser {
         TreeSet<User> result = new TreeSet<>();
         result.addAll(users);
         return result;
+    }
+
+    /**
+     * Сортировка списка пользователей по имени.
+     *
+     * @param users - список пользователей.
+     * @return - отсортированный список.
+     */
+    public List<User> sortNameLength(List<User> users) {
+        users.sort(new NameComparator());
+        return users;
+    }
+
+    /**
+     * Сортировка списка пользователей по имени, а в случае, если имена одинаковые,
+     * то сортировка происходит по возрасту, в порядке возрастания.
+     *
+     * @param users - список пользователей.
+     * @return - отсортированный список.
+     */
+    public List<User> sortByAllFields(List<User> users) {
+        users.sort(new AllFieldsComparator());
+        return users;
+    }
+
+    /**
+     * Класс компоратор - сравнивает двух пользователей по имени.
+     */
+    private class NameComparator implements Comparator<User> {
+        @Override
+        public int compare(User o1, User o2) {
+            return (o1 != null && o2 != null) ? o1.getName().compareTo(o2.getName()) : 0;
+        }
+    }
+
+    /**
+     * Класс компоратор - сравнивает двух пользователей сначала по имени,
+     * а если имена одинаковые, то по возрасту.
+     */
+    private class AllFieldsComparator implements Comparator<User> {
+        @Override
+        public int compare(User o1, User o2) {
+            int result = 0;
+            if (o1 != null && o2 != null) {
+                if (o1.getName().equals(o2.getName())) {
+                    result = o1.getAge() - o2.getAge();
+                } else {
+                    result = o1.getName().compareTo(o2.getName());
+                }
+            }
+            return result;
+        }
     }
 }
--- chapter_002/src/test/java/ru/job4j/sort/TestSortUsers.java
+++ chapter_002/src/test/java/ru/job4j/sort/TestSortUsers.java
@@ -26,4 +26,42 @@ public class TestSortUsers {
         assertThat(iterator.next().getName(), is("Ivan"));
         assertThat(iterator.next().getName(), is("Roman"));
     }
+
+    @Test
+    public void testSortByName() {
+        SortUser sortUser = new SortUser();
+        List<User> users = new ArrayList<>();
+        users.add(new User("Ivan", 17));
+        users.add(new User("Sergey", 12));
+        users.add(new User("Roman", 23));
+        users.add(new User("Igor", 6));
+        sortUser.sortNameLength(users);
+        Iterator<User> iterator = users.iterator();
+        assertThat(iterator.next().getName(), is("Igor"));
+        assertThat(iterator.next().getName(), is("Ivan"));
+        assertThat(iterator.next().getName(), is("Roman"));
+        assertThat(iterator.next().getName(), is("Sergey"));
+    }
+
+    @Test
+    public void testSortByAllFields() {
+        SortUser sortUser = new SortUser();
+        List<User> users = new ArrayList<>();
+        users.add(new User("Ivan", 17));
+        users.add(new User("Ivan", 8));
+        users.add(new User("Sergey", 12));
+        users.add(new User("Sergey", 14));
+        users.add(new User("Sergey", 7));
+        users.add(new User("Roman", 23));
+        users.add(new User("Igor", 6));
+        sortUser.sortByAllFields(users);
+        Iterator<User> iterator = users.iterator();
+        assertThat(iterator.next().getName(), is("Igor"));
+        assertThat(iterator.next().getAge(), is(8));
+        assertThat(iterator.next().getAge(), is(17));
+        assertThat(iterator.next().getName(), is("Roman"));
+        assertThat(iterator.next().getAge(), is(7));
+        assertThat(iterator.next().getAge(), is(12));
+        assertThat(iterator.next().getAge(), is(14));
+    }
 }
