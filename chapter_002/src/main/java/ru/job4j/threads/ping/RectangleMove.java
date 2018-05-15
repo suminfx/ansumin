413710b3abcb369d7ea3607f48b22694ad9de27c
suminfx <sumin931@yandex.ru>
2018-05-15 19:44:48 +0300

    #1016 1. Пин-понг.

---
+++ chapter_002/src/main/java/ru/job4j/threads/ping/RectangleMove.java
@@ -0,0 +1,32 @@
+package ru.job4j.threads.ping;
+
+import javafx.scene.shape.Rectangle;
+
+public class RectangleMove implements Runnable {
+    private final Rectangle rect;
+    private int vSpeed = 3;
+    private int hSpeed = 3;
+
+    public RectangleMove(Rectangle rect) {
+        this.rect = rect;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            if (this.rect.getX() >= 290 || this.rect.getX() <= 0) {
+                hSpeed *= -1;
+            }
+            if (this.rect.getY() >= 290 || this.rect.getY() <= 0) {
+                vSpeed *= -1;
+            }
+            this.rect.setX(this.rect.getX() + hSpeed);
+            this.rect.setY(this.rect.getY() + vSpeed);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
